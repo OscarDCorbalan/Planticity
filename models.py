c37a81916556852d94bd7306b33037f3b9adef20
Oscar Dominguez <oscardc@gmx.com>
2016-04-10 18:26:56 +0200

    feat: Create games

--- models.py
+++ models.py
@@ -1,11 +1,105 @@
+import json
 from google.appengine.ext import ndb
 from protorpc import messages
+
+
+# Load species data
+PLANT_SPECIES = json.loads(open('plant_species.json', 'r').read())['species']
+
+
+# Messages
 
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
     message = messages.StringField(1, required=True)
 
+
+class NewGameForm(messages.Message):
+    """Used to create a new game"""
+    user_name = messages.StringField(1, required=True)
+    
+class GameForm(messages.Message):
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    plant_look = messages.StringField(2, required=True)
+    game_over = messages.BooleanField(3, required=True)
+    message = messages.StringField(4, required=True)
+    user_name = messages.StringField(5, required=True)
+
+
+# ndb models
+
 class User(ndb.Model):
     """User profile"""
     name = ndb.StringProperty(required=True)
     email =ndb.StringProperty()
+
+
+class Plant(ndb.Model):
+    """Plant data"""
+    name = ndb.StringProperty(required=True)
+    common_name = ndb.StringProperty(required=True)
+    age = ndb.IntegerProperty(required=True, default=0)
+    size = ndb.IntegerProperty(required=True, default=0)
+    status = ndb.StringProperty(required=True, default="seed")
+    # place = ndb.StringProperty() # Potted or soil...
+    # light = ndb.StringProperty() #Sun, semi or shadow
+    # Gives hints about moisture, stress...
+    look = ndb.StringProperty()
+    # Internal data, user can't directly see these numbers
+    moisture = ndb.IntegerProperty(required=True, default=0)
+    # plague = ndb.StringProperty()
+    # stress = ndb.IntegerProperty(required=True, default=0)
+
+    @classmethod
+    def new_plant(cls):
+        # TODO add more species, choose randomly
+        variety =  PLANT_SPECIES['pea']
+        print PLANT_SPECIES
+        print variety
+        plant = Plant(name = variety['name'],
+                      common_name = variety['common_name'],
+                      look = "It's a %s seed" %variety['common_name'])
+        plant.put()
+        print 'plant', plant
+        return plant
+
+class Game(ndb.Model):
+    """Game object"""
+    # TODO: history = ndb.????
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    user = ndb.KeyProperty(required=True, kind='User')
+    plant = ndb.KeyProperty(required=True, kind='Plant')
+
+    @classmethod
+    def new_game(cls, user):
+        """Creates and returns a new game"""
+
+        game = Game(user=user,
+                    plant = Plant.new_plant().key,
+                    game_over=False)
+        game.put()
+        print 'game:', game
+        return game
+
+    def to_form(self, message):
+        """Returns a GameForm representation of the Game"""
+        print 'self:', self, '------', message
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.plant_look = self.plant.get().look
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
+    def end_game(self, won=False):
+        """Ends the game - if won is True, the player won. - if won is False,
+        the player lost."""
+        self.game_over = True
+        self.put()
+        # Add the game to the score 'board'
+        score = Score(user=self.user, date=date.today(), won=won,
+                      guesses=self.attempts_allowed - self.attempts_remaining)
+        score.put()
+
--- planticity.py
+++ planticity.py
@@ -1,8 +1,9 @@
 import logging
 import endpoints
 from protorpc import remote, messages
-from models import StringMessage, User
+from models import Game, GameForm, NewGameForm, StringMessage, User
 
+NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
                                            email=messages.StringField(2))
 
@@ -24,6 +25,29 @@ class Planticity(remote.Service):
         user.put()
         return StringMessage(message='User {} created!'.format(
                 request.user_name))
+
+    @endpoints.method(request_message=NEW_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game',
+                      name='new_game',
+                      http_method='POST')
+    def new_game(self, request):
+        """Creates new game"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    'A User with that name does not exist!')
+        try:
+            game = Game.new_game(user.key)
+        except ValueError:
+            raise endpoints.BadRequestException('Maximum must be greater '
+                                                'than minimum!')
+
+        # Use a task queue to update the average attempts remaining.
+        # This operation is not needed to complete the creation of a new game
+        # so it is performed out of sequence.
+        # taskqueue.add(url='/tasks/cache_average_attempts')
+        return game.to_form('Good luck playing Planticity!')
 
    
 api = endpoints.api_server([Planticity])
