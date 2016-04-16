2b6870b54245f45c3d925dd71e67233d687a5e61
Oscar Dominguez <oscardc@gmx.com>
2016-04-16 20:32:11 +0200

    feat: Add fungi infection and fungicide action

--- models/plant.py
+++ models/plant.py
@@ -15,11 +15,12 @@ PLANT = 'plant'
 WAIT = 'wait'
 PLANT_SEED = 'plant seed'
 WATER = 'water'
+FUNGICIDE = 'fungicide'
 # Dictionary of status:[list of possible actions for this status]
 STATUS_ACTIONS = {
     SEED: [
-        WAIT,
-        PLANT_SEED
+        PLANT_SEED,
+        FUNGICIDE
     ],
     PLANTED: [
         WAIT,
@@ -27,7 +28,8 @@ STATUS_ACTIONS = {
     ],
     PLANT:[
         WAIT,
-        WATER
+        WATER,
+        FUNGICIDE
     ]
 }
 
@@ -45,9 +47,12 @@ class Plant(ndb.Model):
     # Internal data, user can't directly see these numbers
     stress = ndb.IntegerProperty()
     moisture = ndb.IntegerProperty(required=True, default=0)
-    # plague = ndb.StringProperty()
-    # stress = ndb.IntegerProperty(required=True, default=0)
+    # % amounts present
+    # fungicide = ndb.IntegerProperty(required=True, default=True)
+    # insecticide = ndb....
+    # Infection markers
     fungi = ndb.BooleanProperty(required=True, default=False)
+    # insects = ndb.StringProperty()
 
     @classmethod
     def new_plant(cls):
@@ -62,6 +67,8 @@ class Plant(ndb.Model):
         plant.put()
         logging.debug('plant', plant)
         return plant
+
+    # Interaction with plant
 
     def interact(self, action):
         logging.debug('do %s in status: %s', action, self.status)
@@ -88,6 +95,9 @@ class Plant(ndb.Model):
         if action == PLANT_SEED:
             logging.debug('_interact_seed: plant seed')
             self._plant_seed()
+        if action == FUNGICIDE:
+            logging.debug('_interact_seed: fungicide seed')
+            self._fungicide()
 
     def _interact_planted(self, action):
         if action == WATER:
@@ -98,30 +108,52 @@ class Plant(ndb.Model):
         if action == WATER:
             logging.debug('_interact_plant: water plant')
             self._water()
+        if action == FUNGICIDE:
+            logging.debug('_interact_plant: fungicide plant')
+            self._fungicide()
 
     def end_day(self):
         data = PLANT_SPECIES[self.name]
 
         self.age += 1
+
+        # Reduce soil moisture
         lost_water = random.randint(10, 20)
         self.moisture = max(self.moisture - lost_water, 0)
+
+        # Advance plant stages
         if self.age == data['days']['germinate']:
             self._germinate()
+
+        # Let the Nature do its miracle (cell mitosis)
         if self.status == PLANT:
             # Add % of growth_rate
             day_growth = 0.01 * (100-self.stress) * data['growth_rate']
             logging.debug('end_day growth: %s', day_growth)
             self.size += int(day_growth)
+
+        # Roll fungi chance
+        if self.status != SEED and not self.fungi:
+            fungi_chance = data[self.status]['fungi_chance']
+            dice = random.randint(0, 100)
+            if fungi_chance > dice:
+                self.fungi = True
+
+        # Adjust plant stress
         if self.status == PLANT:
             moisture_diff = abs(self.moisture - data['ideal_moisture'])
             logging.debug('end_day moisture_diff: %s', moisture_diff)
             self.stress = self.stress + moisture_diff - 30
+            if self.fungi:
+                self.stress += 25
             self.stress = min(self.stress, 100)
             self.stress = max(self.stress, 0)
+        
         self._update_look()
         self.put()
 
     # Methods that progress the status of the plant
+
     def _plant_seed(self):
         if self.status != SEED:
             raise ValueError('You have already planted your seed')
@@ -140,6 +172,10 @@ class Plant(ndb.Model):
         logging.debug('_water cur:%s ret:%s', self.moisture, retained_water)
         self.moisture = min(self.moisture + retained_water, 100)
 
+    def _fungicide(self):
+        logging.debug('_fungicide()')
+        self.fungi = False
+
     def _update_look(self):
         looks = []  # Append texts and finally join them in a single string
         looks.append('Day %s' % self.age)
@@ -156,6 +192,8 @@ class Plant(ndb.Model):
 
         if self.status in [PLANT]:  #, MATURE]:
             looks.append('It measures %s cm' % self.size)
+            if self.fungi:
+                looks.append('The plant got fungi! You should use fungicide')
 
         if self.moisture == 0:
             looks.append('The soil is completely dry')
