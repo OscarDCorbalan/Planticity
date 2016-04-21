363a249f598355fd60022b4be84d2c443be552bd
Oscar Dominguez <oscardc@gmx.com>
2016-04-21 17:13:05 +0200

    docs: Add class and method docstrings

--- models/score.py
+++ models/score.py
@@ -2,13 +2,17 @@ from google.appengine.ext import ndb
 
 
 class Score(ndb.Model):
-    """Score object"""
+    """Represents a Score as an ndb Model"""
     user = ndb.KeyProperty(required=True, kind='User')
     date = ndb.DateProperty(required=True)
     won = ndb.BooleanProperty(required=True)
     harvest = ndb.IntegerProperty(required=True)
 
     def to_form(self):
+        """Returns a ScoreForm representation of the Score.
+
+        Returns:
+            A ScoreForm object"""
         return ScoreForm(user_name=self.user.get().name,
                          won=self.won,
                          date=str(self.date),
