36fbdc0a6961598ea46f71380f61bd62560dd788
Oscar Dominguez <oscardc@gmx.com>
2016-04-22 20:03:59 +0200

    fix: Whitespace

--- models/user.py
+++ models/user.py
@@ -4,4 +4,4 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     """Represents a User as an ndb Model"""
     name = ndb.StringProperty(required=True)
-    email =ndb.StringProperty()
+    email = ndb.StringProperty()
